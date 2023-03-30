ef06245732c0ead494f61d0db25e6743371ab929
vehymy <selimhanbaltas@gmail.com>
2023-03-30 11:23:21 +0300

    JS codes added.

---
+++ script.js
@@ -0,0 +1,11 @@
+const skills = [
+    { name: "VueJS", image: "images/Vue.png" },
+    { name: "Spring Boot", image: "images/springboot.png" },
+    { name: "CSS", image: "images/css.png" },
+    { name: "JavaScript", image: "images/js.png" },
+    { name: "Java", image: "images/java.png" },
+    { name: "Python", image: "images/python.png" },
+  ];
+
+
+  
