fee7da1b7ce74f0c1ca45091f124b83906cbe7a6
vehymy <selimhanbaltas@gmail.com>
2023-05-06 00:41:07 +0300

    Skills page updated

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -15,7 +15,8 @@ const skillsDiv = document.getElementById('skills');
 for (let i = 0; i < skillsArray.length; i++) {
     const skill = skillsArray[i];
     console.log(skill)
-    const skillHtml = `<div class="skillc px-1-xl px-md-5"> <div class="skill"> <img src="${skill.image}" alt=""> <h3>${skill.name}</h3> </div> </div>`;
+    const asd = `<div class="skillc px-1-xl px-md-5"> <div class="skill"> <img class="skillImage" src="${skill.image}" alt=""> <h3>${skill.name}</h3> </div> </div>`;
+    const skillHtml = `<div class="skillc px-1-xl px-md-5">  <div title="${skill.name}" class="skill position-relative"><img src="assets/images/badge.png" class="myBadge" style="z-index: -1;" alt=""><img class="skillImage position-absolute" src="${skill.image}" alt=""></div></div></div>`
     skillsDiv.insertAdjacentHTML("beforeend", skillHtml);
 }
 
